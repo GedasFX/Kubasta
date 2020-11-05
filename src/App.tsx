ac6415e94f53bf99d9aa5b03f45e7ad78def0eca
Gediminas Milašius <gediminasmilasius@gmail.com>
2020-11-05 23:18:35 +0200

    quizagme initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
+import {
+  AppBar,
+  Button,
+  Container,
+  CssBaseline,
+  IconButton,
+  ThemeProvider,
+  Toolbar,
+} from '@material-ui/core';
+import { Menu } from '@material-ui/icons';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.css';
+import theme from './theme';
+import QuizGame from './views/QuizGame';
+
+import image from './bg/ee72e41123a09d160822985d61668c18.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <Menu />
+            </IconButton>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+        <Container maxWidth="lg">
+          <QuizGame
+            imageUrl={image}
+            buttons={[
+              { position: { top: '10%', left: '10%' }, size: { width: '20%', height: '20%' } },
+              { position: { top: '40%', left: '10%' }, size: { width: '20%', height: '20%' } },
+            ]}
+          />
+        </Container>
+      </ThemeProvider>
+    </>
   );
 }
 
