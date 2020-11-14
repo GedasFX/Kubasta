e50f26af0e5a6dde1f3e684130b12b49715e288d
Olivier Levasseur <olivier.jules.levasseur@gmail.com>
2020-11-14 21:48:14 +0200

    Add screens up until network profile selection

--- src/game-data/task1.ts
+++ src/game-data/task1.ts
@@ -57,6 +57,7 @@ const task1data: TaskData = {
     pressonsecurenetwork: {
       component: fromUrl(pressonsecurenetwork),
       buttons: [
+          //network icon in desktop bar
         {
           position: {left: '92%', top: '94.5%'},
           size: {width: '2%', height: '4%'},
@@ -64,20 +65,89 @@ const task1data: TaskData = {
             dispatch(gameActions.setActiveScreenId({id: 'desktopnetworknotconnected'}));
           },
         },
+          //connect button
+        {
+          position: {top: '48.5%', left: '89.5%'},
+          size: {width: '9.5%', height: '4%'},
+          onClick: (dispatch)=> {
+            dispatch(gameActions.setActiveScreenId({id: 'typeinpassword'}));
+          }
+        },
+          //open network button
+        {
+          position: {top: '28.1%', left: '76.4%'},
+          size: {width: '24%', height: '8%'},
+          onClick: (dispatch)=> {
+            dispatch(gameActions.setActiveScreenId({id: 'pressonopennetwork'}));
+          }
+        },
       ]
     },
     pressonopennetwork: {
       component: fromUrl(pressonopennetwork),
       buttons: [
+          //network icon in desktop bar
         {
-          position: {top: '10%', left: '10%'},
-          size: {width: '20%', height: '20%'},
+          position: {left: '92%', top: '94.5%'},
+          size: {width: '2%', height: '4%'},
           onClick: (dispatch) => {
-            dispatch(gameActions.setActiveScreenId({id: 'Second'}));
+            dispatch(gameActions.setActiveScreenId({id: 'desktopnetworknotconnected'}));
           },
         },
-
+          //secure network button
+        {
+          position: {left: '76.6%', top: '50.9%'},
+          size: {width: '24%', height: '8%'},
+          onClick: (dispatch) => {
+            dispatch(gameActions.setActiveScreenId({id: 'pressonsecurenetwork'}));
+          },
+        },
+          //connect button
+        {
+          position: {left: '89.5%', top: '45.45%'},
+          size: {width: '9.5%', height: '3.5%'},
+          onClick: (dispatch) => {
+            dispatch(gameActions.setActiveScreenId({id:'connectedopennetwork'}));
+          }
+        },
       ]
+    },
+    typeinpassword: {
+      component: fromUrl(typeinpassword),
+      buttons: [
+        //network icon in desktop bar
+        {
+          position: {left: '92%', top: '94.5%'},
+          size: {width: '2%', height: '4%'},
+          onClick: (dispatch) => {
+            dispatch(gameActions.setActiveScreenId({id: 'desktopnetworknotconnected'}));
+          },
+        },
+        //open network button
+        {
+          position: {top: '28.1%', left: '76.4%'},
+          size: {width: '24%', height: '8%'},
+          onClick: (dispatch)=> {
+            dispatch(gameActions.setActiveScreenId({id: 'pressonopennetwork'}));
+          }
+        },
+        //next button (after password entered)
+        {
+          position: {top: '52.1%', left: '79.7%'},
+          size: {width: '9.7%', height: '3.8%'},
+          onClick: (dispatch)=> {
+            dispatch(gameActions.setActiveScreenId({id: 'connectedsecurenetwork'}));
+          }
+        },
+      ],
+    },
+    connectedopennetwork: {
+      component:fromUrl(connectedopennetwork),
+      buttons:[],
+    },
+    connectedsecurenetwork: {
+      component:fromUrl(connectedsecurenetwork),
+      buttons:[],
     }
   }
 };
