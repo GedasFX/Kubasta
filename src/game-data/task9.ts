7e3d3d807ed50ae84ce05e2469db6fdcc8cfa6ed
Olivier Levasseur <olivier.jules.levasseur@gmail.com>
2020-11-25 19:27:29 +0200

    Add sidebar buttons for tasks 7,8,9

--- src/game-data/task9.ts
+++ src/game-data/task9.ts
@@ -67,6 +67,87 @@ const task9data: TaskData = {
       ],
     },
   },
+    buttons: [
+        {
+            text: 'Call',
+            onClick:({dispatch}) => {
+                dispatch(
+                    gameActions.openFeedbackDialog({
+                        text:
+                            'Smart! If you ever need to share password, it is good to use a phone call, so your password doesn’t get written down and roam the depths of internet.',
+                        next: {
+                            taskId: 10,
+                            screenId: 'facebook',
+                        },
+                    })
+                );
+                dispatch(gameActions.updatePoints({ points: 5 }));
+            }
+        },
+        {
+            text: 'Send via Whatsapp',
+            onClick: ({dispatch}) => {
+                dispatch(
+                    gameActions.openFeedbackDialog({
+                        text:
+                            'Good! Choosing another communication channel is a good way of avoiding unwanted password theft. However, not writing your password would be a better choice',
+                        next: {
+                            taskId: 10,
+                            screenId: 'facebook',
+                        },
+                    })
+                );
+                dispatch(gameActions.updatePoints({ points: 5 }));
+            }
+        },
+        {
+            text: 'Send via email',
+            onClick: ({dispatch}) => {
+                dispatch(
+                    gameActions.openFeedbackDialog({
+                        text:
+                            'Even though this is a genuine email, you should never send your password to anyone in plaintext as they might get stolen.',
+                        next: {
+                            taskId: 10,
+                            screenId: 'facebook',
+                        },
+                    })
+                );
+                dispatch(gameActions.updatePoints({ points: -5 }));
+            }
+        },
+        {
+            text: 'Ignore',
+            onClick: ({dispatch}) => {
+                dispatch(
+                    gameActions.openFeedbackDialog({
+                        text: 'It is not nice to ignore your friend’s emails like that! Better contact her via different communication channel.',
+                        next: {
+                            taskId: 10,
+                            screenId: 'facebook',
+                        },
+                    })
+                );
+                dispatch(gameActions.updatePoints({ points: 0 }));
+            }
+        },
+        {
+            text: 'Report spam',
+            onClick: ({dispatch}) => {
+                dispatch(
+                    gameActions.openFeedbackDialog({
+                        text:
+                            'Reporting genuine emails as spam messes up your email spam filtering algorithms, and you may miss some important future emails.',
+                        next: {
+                            taskId: 10,
+                            screenId: 'facebook',
+                        },
+                    })
+                );
+                dispatch(gameActions.updatePoints({ points: -10 }));
+            }
+        }
+    ]
 };
 
 export default task9data;
